55ae48d546b279a3b939ce988f5caf681c51cd22
xhikzerox@gmail.com <xhikzerox@gmail.com>
2024-07-30 03:15:23 +0500

    + Small changes

--- index.js
+++ index.js
@@ -84,5 +84,5 @@ function createBot(nickname, password, portal, warp, sendAd, adMsg, defX, defY, 
 }
 
 
-createBot("Kemper1ng", "!afterHuila00pidor3svocvoRus", "s2", "chbot", true, adDefMsg, -4871.5, 109, -3179.5)
+// createBot("Kemper1ng", "!afterHuila00pidor3svocvoRus", "s2", "chbot", true, adDefMsg, -4871.5, 109, -3179.5)
 createBot("SCPbotSH", "!afterHuila00pidor3svocvoRus", "s4", "chbot", true, adDefMsg2, -4870.5, 109, -3178.5)
