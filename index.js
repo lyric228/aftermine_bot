b0f6457d3d4b63e7a312bf5d25124bb6fa917a72
xhikzerox@gmail.com <xhikzerox@gmail.com>
2024-08-04 04:52:10 +0500

    + Auto-restart in 1 hour + Auto-restart on /freezetroll bot + More random in ad delay + Player invite delay random + Ad msg fixes + Message log feature added (commented by default) - SendAd arg deleted - Commented old ad message

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const mineflayer = require("mineflayer");
 
-const adDefMsg = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Мы выдаём флай игрокам :3. Чего же ты ждёшь? Присоединяйся к нам!";
-const adDefMsg2 = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Чего же ты ждёшь? Присоединяйся к нам!";
-const blacklist = ["uzerchik", "Milaina"];
+// const adDefMsgOld = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Мы выдаём флай игрокам :3. Чего же ты ждёшь? Присоединяйся к нам!";
+const adDefMsg = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Чего же ты ждёшь? Присоединяйся к нам!";
+const blacklist = ["uzerchik", "Milaina", "Диего_санчез"];
+const cheat_check = "[system]: Пожайлуста прекратите читерить или вы будете забанены!";
 
 
 function getRandomNumber(min, max) {
@@ -29,7 +30,7 @@ function invitePlayers(bot) {
     if (closestPlayer && !blacklist.includes(closestPlayer.username)) {
       bot.chat(`/c invite ${closestPlayer.username}`);
     }
-  }, 10000);
+  }, getRandomNumber(1000, 15000));
 }
 
 function lookAtEntities(bot) {
@@ -42,13 +43,11 @@ function lookAtEntities(bot) {
   }, 10);
 }
 
-function sendAdvertisements(bot, sendAd, adMsg) {
+function sendAdvertisements(bot, adMsg) {
   setInterval(() => {
-    if (sendAd) {
-      bot.chat(adMsg);
-    }
     bot.chat("/clear");
-  }, getRandomNumber(160000, 200000));
+    bot.chat(adMsg);
+  }, getRandomNumber(150000, 180000));
 }
 
 function monitorPosition(bot, defCord, warp) {
@@ -60,27 +59,39 @@ function monitorPosition(bot, defCord, warp) {
   }, 1000);
 }
 
-function createBot(nickname, password, portal, warp, sendAd, adMsg, defCord) {
+function createBot(nickname, password, portal, warp, adMsg, defCord) {
   const bot = mineflayer.createBot({
     host: "mc.masedworld.net",
     port: 25565,
     username: nickname,
   });
 
+  setTimeout(() => {
+    bot.end();
+  }, 60*60*1000);  // Рестарт бота раз в 1 час
+
   bot.on("spawn", () => handleSpawn(bot, password, portal));
+
   bot.once("spawn", () => {
     invitePlayers(bot);
     lookAtEntities(bot);
-    sendAdvertisements(bot, sendAd, adMsg);
+    sendAdvertisements(bot, adMsg);
     monitorPosition(bot, defCord, warp);
   });
 
+  bot.on("message", (jsonMsg, position) => {
+    const message = `[${position}]: ${jsonMsg.toString()}`;
+    // console.log(message);  // Лог сообщений
+    if (message.includes(cheat_check)) bot.end();
+  });
+
   bot.on("error", (err) => console.log(err));
+
   bot.on("end", () => {
     console.log(`${nickname} - Reconnection...`);
-    createBot(nickname, password, portal, warp, sendAd, adMsg, defCord);
+    createBot(nickname, password, portal, warp, adMsg, defCord);
   });
 }
 
-createBot("Kemper1ng", "!afterHuila00pidor3svocvoRus", "s2", "CHbot1", true, adDefMsg, [9105.5, 169, 10104.5])
-createBot("SCPbotSH", "!afterHuila00pidor3svocvoRus", "s3", "chbot", true, adDefMsg2, [-4871.5, 109, -3179.5])
+createBot("Kemper1ng", "!afterHuila00pidor3svocvoRus", "s2", "CHbot1", adDefMsg, [9105.5, 169, 10104.5])
+createBot("SCPbotSH", "!afterHuila00pidor3svocvoRus", "s3", "chbot", adDefMsg, [-4871.5, 109, -3179.5])
