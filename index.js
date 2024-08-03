aa58084c83babd511f455341c42add450957ceac
xhikzerox@gmail.com <xhikzerox@gmail.com>
2024-08-03 23:14:51 +0500

    + Filter players from other entities + Kemper1ng new warp and pos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const mineflayer = require('mineflayer');
+const mineflayer = require("mineflayer");
 
-const adDefMsg = '!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Мы выдаём флай игрокам :3. Чего же ты ждёшь? Присоединяйся к нам!';
+const adDefMsg = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Мы выдаём флай игрокам :3. Чего же ты ждёшь? Присоединяйся к нам!";
 const adDefMsg2 = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Чего же ты ждёшь? Присоединяйся к нам!";
 const blacklist = ["uzerchik", "Milaina"];
 
@@ -25,8 +25,8 @@ function handleSpawn(bot, password, portal) {
 
 function invitePlayers(bot) {
   setInterval(() => {
-    const closestPlayer = bot.nearestEntity();
-    if (closestPlayer && closestPlayer.type === 'player' && !blacklist.includes(closestPlayer.username)) {
+    const closestPlayer = bot.nearestEntity(entity => entity.type === "player");
+    if (closestPlayer && !blacklist.includes(closestPlayer.username)) {
       bot.chat(`/c invite ${closestPlayer.username}`);
     }
   }, 10000);
@@ -34,10 +34,9 @@ function invitePlayers(bot) {
 
 function lookAtEntities(bot) {
   setInterval(() => {
-    const entity = bot.nearestEntity();
-    // const nearestPlayer = bot.nearestEntity(bot.players, { maxDistance: 16 });
-    if (entity) {
-      const lookPosition = entity.type === 'player' ? entity.position.offset(0, 1.6, 0) : entity.position;
+    const closestPlayer = bot.nearestEntity(entity => entity.type === "player");
+    if (closestPlayer) {
+      let lookPosition = closestPlayer.position.offset(0, 1.6, 0)
       bot.lookAt(lookPosition);
     }
   }, 10);
@@ -48,7 +47,7 @@ function sendAdvertisements(bot, sendAd, adMsg) {
     if (sendAd) {
       bot.chat(adMsg);
     }
-    bot.chat('/clear');
+    bot.chat("/clear");
   }, getRandomNumber(160000, 200000));
 }
 
@@ -63,25 +62,25 @@ function monitorPosition(bot, defCord, warp) {
 
 function createBot(nickname, password, portal, warp, sendAd, adMsg, defCord) {
   const bot = mineflayer.createBot({
-    host: 'mc.masedworld.net',
+    host: "mc.masedworld.net",
     port: 25565,
     username: nickname,
   });
 
-  bot.on('spawn', () => handleSpawn(bot, password, portal));
-  bot.once('spawn', () => {
+  bot.on("spawn", () => handleSpawn(bot, password, portal));
+  bot.once("spawn", () => {
     invitePlayers(bot);
     lookAtEntities(bot);
     sendAdvertisements(bot, sendAd, adMsg);
     monitorPosition(bot, defCord, warp);
   });
 
-  bot.on('error', (err) => console.log(err));
-  bot.on('end', () => {
+  bot.on("error", (err) => console.log(err));
+  bot.on("end", () => {
     console.log(`${nickname} - Reconnection...`);
     createBot(nickname, password, portal, warp, sendAd, adMsg, defCord);
   });
 }
 
-createBot("Kemper1ng", "!afterHuila00pidor3svocvoRus", "s2", "botch", true, adDefMsg, [-4871.5, 109, -3179.5])
+createBot("Kemper1ng", "!afterHuila00pidor3svocvoRus", "s2", "CHbot1", true, adDefMsg, [9105.5, 169, 10104.5])
 createBot("SCPbotSH", "!afterHuila00pidor3svocvoRus", "s3", "chbot", true, adDefMsg2, [-4871.5, 109, -3179.5])
