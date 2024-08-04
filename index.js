a8aa921253aafea184b42d5e10440cd8e3a0ce4a
xhikzerox@gmail.com <xhikzerox@gmail.com>
2024-08-04 21:28:36 +0500

    + Fixes for new /s7 bot

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const mineflayer = require("mineflayer");
 
 // const adDefMsgOld = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Мы выдаём флай игрокам :3. Чего же ты ждёшь? Присоединяйся к нам!";
-const adDefMsg = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Чего же ты ждёшь? Присоединяйся к нам!";
+const adDefMsg1 = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Чего же ты ждёшь? Присоединяйся к нам!";
+const adDefMsg2 = "!&c&lПриветик! Хочешь с кайфом провести время, но не знаешь как? Тогда тебе подойдёт клан &4&lChert&0&lHouse &c&l! У нас ты найдёшь хороший кх, топовый кит и уважение клана. Чтоб вступить в клан пиши /warp CH"
+const adDefMsg3 = "!&c&lХочешь в крутой клан с многими плюшками? Тогда тебе нужен клан &4&lChert&0&lHouse&c&l ! У нас ты не только найдёшь топовый кит для пвп и хороший кх, но и дс сервер! А так же у нас открыт набор на модераторов! /warp CH"
+const adMessages = [adDefMsg1, adDefMsg2, adDefMsg3];
 const blacklist = ["uzerchik", "Milaina", "Диего_санчез"];
 const cheat_check = "[system]: Пожайлуста прекратите читерить или вы будете забанены!";
 
@@ -43,10 +46,10 @@ function lookAtEntities(bot) {
   }, 10);
 }
 
-function sendAdvertisements(bot, adMsg) {
+function sendAdvertisements(bot, adMsgs) {
   setInterval(() => {
     bot.chat("/clear");
-    bot.chat(adMsg);
+    bot.chat(adMsgs[getRandomNumber(0, adMsgs.length)]);
   }, getRandomNumber(150000, 180000));
 }
 
@@ -59,7 +62,7 @@ function monitorPosition(bot, defCord, warp) {
   }, 1000);
 }
 
-function createBot(nickname, password, portal, warp, adMsg, defCord) {
+function createBot(nickname, password, portal, warp, adMsgs, defCord) {
   const bot = mineflayer.createBot({
     host: "mc.masedworld.net",
     port: 25565,
@@ -75,7 +78,7 @@ function createBot(nickname, password, portal, warp, adMsg, defCord) {
   bot.once("spawn", () => {
     invitePlayers(bot);
     lookAtEntities(bot);
-    sendAdvertisements(bot, adMsg);
+    sendAdvertisements(bot, adMsgs);
     monitorPosition(bot, defCord, warp);
   });
 
@@ -89,10 +92,10 @@ function createBot(nickname, password, portal, warp, adMsg, defCord) {
 
   bot.on("end", () => {
     console.log(`${nickname} - Reconnection...`);
-    createBot(nickname, password, portal, warp, adMsg, defCord);
+    createBot(nickname, password, portal, warp, adMsgs, defCord);
   });
 }
 
-createBot("Kemper1ng", "!afterHuila00pidor3svocvoRus", "s2", "CHbot1", adDefMsg, [9105.5, 169, 10104.5])
-createBot("SCPbotSH", "!afterHuila00pidor3svocvoRus", "s3", "chbot", adDefMsg, [-4871.5, 109, -3179.5])
-createBot("Alfhelm", "!afterHuila00pidor3svocvoRus", "s7", "chbot", adDefMsg, [-4871.5, 109, -3179.5])
+createBot("Kemper1ng", "!afterHuila00pidor3svocvoRus", "s2", "CHbot1", adMessages, [9105.5, 169, 10104.5])
+createBot("SCPbotSH", "!afterHuila00pidor3svocvoRus", "s3", "chbot", adMessages, [-4871.5, 109, -3179.5])
+createBot("Alfhelm", "!afterHuila00pidor3svocvoRus", "s7", "chbot", adMessages, [-10206.5, 159, -13028.5])  // cords
