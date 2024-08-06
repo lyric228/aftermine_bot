15bf8c306746a0d9d4782f6e170b25e28edd2261
xhikzerox@gmail.com <xhikzerox@gmail.com>
2024-08-06 22:25:42 +0500

    + New warps for all portals + /cc messages on leave or join for player + Message monitor better code view

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mineflayer = require("mineflayer");
 
 // const adDefMsgOld = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Мы выдаём флай игрокам :3. Чего же ты ждёшь? Присоединяйся к нам!";
-const adDefMsg1 = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp ChertHouse ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Чего же ты ждёшь? Присоединяйся к нам!";
-const adDefMsg2 = "!&c&lПриветик! Хочешь с кайфом провести время, но не знаешь как? Тогда тебе подойдёт клан &4&lChert&0&lHouse &c&l! У нас ты найдёшь хороший кх, топовый кит и уважение клана. Чтоб вступить в клан пиши /warp ChertHouse"
-const adDefMsg3 = "!&c&lХочешь в крутой клан с многими плюшками? Тогда тебе нужен клан &4&lChert&0&lHouse&c&l ! У нас ты не только найдёшь топовый кит для пвп и хороший кх, но и дс сервер! А так же у нас открыт набор на модераторов! /warp ChertHouse"
+const adDefMsg1 = "!&c&lПривет, друг! Хочешь побывать в клане, где была великая история? Тогда тебе сюда -> /warp CH или /warp ChertHouse ! У нас есть: топовый кит для пвп, хороший кх и многое другое! Чего же ты ждёшь? Присоединяйся к нам!";
+const adDefMsg2 = "!&c&lПриветик! Хочешь с кайфом провести время, но не знаешь как? Тогда тебе подойдёт клан &4&lChert&0&lHouse &c&l! У нас ты найдёшь хороший кх, топовый кит и уважение клана. Чтоб вступить в клан пиши /warp CH или /warp ChertHouse"
+const adDefMsg3 = "!&c&lХочешь в крутой клан с многими плюшками? Тогда тебе нужен клан &4&lChert&0&lHouse&c&l ! У нас ты не только найдёшь топовый кит для пвп и хороший кх, но и дс сервер! А так же у нас открыт набор на модераторов! /warp CH или /warp ChertHouse"
 const adMessages = [adDefMsg1, adDefMsg2, adDefMsg3];
-const blacklist = ["uzerchik", "Milaina", "Диего_санчез", "TimohaFriend638"];
+const blacklist = ["uzerchik", "Milaina", "Диего_санчез", "TimohaFriend638", "0fansik", "menvixss", "pro7070"];
 const cheat_check = "[system]: Пожайлуста прекратите читерить или вы будете забанены!";
 
 
@@ -62,6 +62,22 @@ function monitorPosition(bot, defCord, warp) {
   }, 1000);
 }
 
+function messagesMonitoring(position, jsonMsg, bot) {
+  const message = `[${position}]: ${jsonMsg.toString()}`;
+  // console.log(message);  // Лог сообщений
+  const matchLeave = message.match(/› (.*?) покинул клан\./);
+  const matchJoin = message.match(/› (.*?) присоеденился к клану\./);
+  if (matchJoin) {
+    const new_member = matchJoin[1];
+    bot.chat(`/cc Добро пожаловать в клан, ${new_member}! Обязательно вступи в наш дискорд, там много всего интересного! Ссылка на дискорд находится в /c infо`);
+  }
+  if (matchLeave) {
+    const leave_member = matchLeave[1];
+    bot.chat(`/cc ${leave_member} выходит из клана, на штык его!`);
+  }
+  if (message.includes(cheat_check)) bot.end();
+}
+
 function createBot(nickname, password, portal, warp, adMsgs, defCord) {
   const bot = mineflayer.createBot({
     host: "mc.masedworld.net",
@@ -75,27 +91,16 @@ function createBot(nickname, password, portal, warp, adMsgs, defCord) {
 
   bot.on("spawn", () => handleSpawn(bot, password, portal));
 
+  bot.on("message", (jsonMsg, position ) => messagesMonitoring(position, jsonMsg, bot));
+
   bot.once("spawn", () => {
     invitePlayers(bot);
     lookAtEntities(bot);
     sendAdvertisements(bot, adMsgs);
     monitorPosition(bot, defCord, warp);
   });
-
-  bot.on("message", (jsonMsg, position) => {
-    const message = `[${position}]: ${jsonMsg.toString()}`;
-    // console.log(message);  // Лог сообщений
-    if (message.includes(cheat_check)) bot.end();
-  });
-
-  bot.on("error", (err) => console.log(err));
-
-  bot.on("end", () => {
-    console.log(`${nickname} - Reconnection...`);
-    createBot(nickname, password, portal, warp, adMsgs, defCord);
-  });
 }
 
-createBot("Kemper1ng", "!afterHuila00pidor3svocvoRus", "s2", "CHbot1", adMessages, [9105.5, 169, 10104.5])
-createBot("SCPbotSH", "!afterHuila00pidor3svocvoRus", "s3", "chbot", adMessages, [-4871.5, 109, -3179.5])
-createBot("Alfhelm", "!afterHuila00pidor3svocvoRus", "s7", "chbot", adMessages, [-10206.5, 159, -13028.5])  // cords
+createBot("Kemper1ng", "!afterHuila00pidor3svocvoRus", "s2", "nf9akf30k", adMessages, [9105.5, 169, 10104.5])
+createBot("SCPbotSH", "!afterHuila00pidor3svocvoRus", "s3", "nf9akf30k", adMessages, [-4871.5, 109, -3179.5])
+createBot("Alfhelm", "!afterHuila00pidor3svocvoRus", "s7", "nf9akf30k", adMessages, [-10206.5, 159, -13028.5])  // cords
