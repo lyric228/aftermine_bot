847b55768a4ae8e89d9be851e7ec9c4d67b4dea0
Lyric <154961945+lyric228@users.noreply.github.com>
2024-10-01 23:34:03 +0500

    (Забыл добавить новые файлы)

---
+++ start.mjs
@@ -0,0 +1,17 @@
+import { createBotCH, clearConsole } from "./index.mjs";
+// import { createBotUha } from "./uha.mjs";
+
+clearConsole(10*60);
+// Создание ботов
+//
+createBotCH("VectorKemper1ng", "s1");
+createBotCH("Kemper1ng", "s2");
+createBotCH("NeoKemper1ng", "s3");
+createBotCH("SCPbotSH", "s4");
+createBotCH("Alfhelm", "s5");
+createBotCH("QuaKemper1ng", "s6");
+createBotCH("AntiKemper1ng", "s7");
+createBotCH("Temper1ng", "s8");
+// Создание бота ухи
+//
+//createBotUha("M2ndar1HoBbIui", "s2", "n9831knt", false, "yagey");
